4bf3db53c89e9d7a3451bf4bfdfb97280f6650f0
Alexis Zamanidis <alexiszamanidis@outlook.com>
2021-07-20 23:54:09 +0300

    add vscode extensions update to update_and_clean_up.sh script

--- update_and_clean_up.sh
+++ update_and_clean_up.sh
@@ -9,6 +9,14 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
+colorful_echo "Updating VSCODE extensions..." "GREEN"
+code --list-extensions | 
+while read extension;
+do
+    colorful_echo "Updating $extension..." "GREEN"
+    code --install-extension $extension --force
+done
+
 # clean up
 colorful_echo "Cleaning Cache..." "GREEN"
 sudo apt-get clean -y
@@ -18,5 +26,4 @@ colorful_echo "Removing Unnecessary Packages..." "GREEN"
 sudo apt-get autoremove -y
 
 colorful_echo "Cleaning user and root trash..." "GREEN"
-sudo trash-empty
 trash-empty
