2e57a72ec8e7e0d00258d08f6181fc4e22b7418d
Alexis Zamanidis <alexiszamanidis@outlook.com>
2021-10-13 19:00:39 +0300

    add chrome extensions installation script

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@ cd ~/.ubuntu_bootstrap
 
 bash install_packages.sh
 
-# if the running system is not WSL run the applications installation script
+# if the running system is not WSL run the applications
+# and the chrome extensions installation scripts
 if [[ ! -n "$IS_WSL" && ! -n "$WSL_DISTRO_NAME" ]]; then
     bash install_applications.sh
+    sudo bash install_chrome_extensions.sh
 fi
 
 bash update_and_clean_up.sh
 
-echo $OLDPWD
+cd $OLDPWD
---
+++ install_chrome_extensions.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# load functions.sh
+source ./functions.sh
+
+install_chrome_extension () {
+  name=$1
+  id=$2
+  preferences_dir_path="/opt/google/chrome/extensions"
+  upd_url="https://clients2.google.com/service/update2/crx"
+
+  mkdir -p "$preferences_dir_path"
+  pref_file_path="$preferences_dir_path/${id}.json"
+
+  echo "{" > "$pref_file_path"
+  echo "  \"external_update_url\": \"$upd_url\"" >> "$pref_file_path"
+  echo "}" >> "$pref_file_path"
+
+  colorful_echo "${name}" "GREEN"
+}
+
+colorful_echo "Installing Chrome Extensions..." "GREEN"
+
+# name - id
+install_chrome_extension "AdBlock" "gighmmpiobklfepjocnamgkkbiglidom"
+install_chrome_extension "Cookie Editor" "iphcomljdfghbkdcfndaijbokpgddeno"
+install_chrome_extension "daily.dev" "jlmpjdjjbgclbocgajdjefcidcncaied"
+install_chrome_extension "EditThisCookie" "fngmhnnpilhplaeedifhccceomclgfbg"
+install_chrome_extension "Eye Dropper" "hmdcmlfkchdmnmnmheododdhjedfccka"
+install_chrome_extension "ModHeader" "idgpnmonknjnojddfkpgkljpfnnfcklj"
+install_chrome_extension "React Developer Tools" "fmkadmapgofadopljbjfkapdkoienihi"
+install_chrome_extension "Video Speed Controller" "nffaoalbilbmmfgbnbgppjihopabppdk"
+install_chrome_extension "Wappalyzer" "gppongmhjkpfnbhagpmjfkannfbllamg"
+install_chrome_extension "JSON Viewer" "gbmdgpbipfallnflgajpaliibnhdgobh"
+install_chrome_extension "Grammarly" "kbfnbcaeplbcioakkpcpgfkobkghlhen"
