98e34110cab4fefed71131c11875366bb014263b
Alexis Zamanidis <alexis.zamanidis@upstreamsystems.com>
2021-10-20 19:00:11 +0300

    fix applications installation script

--- scripts/install_applications.sh
+++ scripts/install_applications.sh
@@ -12,17 +12,17 @@ sudo apt-get dist-upgrade -y
 input="../txt_files/applications.txt"
 while IFS= read -r line
 do
-    apt_install $line
+    snap_install $line
 done < "$input"
 
 # install vscode
 sudo snap install --classic code
 
 # install all the vscode extensions
-input="../txt_files/applications.txt"
+input="../txt_files/vscode_extensions.txt"
 while IFS= read -r line
 do
-    colorful_echo "code --install-extension $line"
+    colorful_echo "code --install-extension $line" "GREEN"
     code --install-extension $line
 done < "$input"
 
