d5871062eecf5e18f8cbb190db8351d0b5408798
Alexis Zamanidis <alexis.zamanidis@upstreamsystems.com>
2021-11-12 23:45:58 +0200

    add i3

--- scripts/config.sh
+++ scripts/config.sh
@@ -7,6 +7,13 @@ declare -a packages=("kitty" "neovim")
 
 install_packages packages
 
+declare -a i3_packages=("i3" "rofi" "compton" "compton" "i3blocks" "feh")
+
+install_packages i3_packages
+
+mkdir ~/.fonts
+cp -R ../fonts/. ~/.fonts/
+
 cp -R ../config/. ~/.config/
 
 exit 0
